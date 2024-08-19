d7e312b9eae8d45968b1d5d1935d1572918f7f1e
zinee <zinee1@naver.com>
2024-08-19 16:42:41 +0900

    팝업에 스크롤이랑 재료 추가

--- script.js
+++ script.js
@@ -4,32 +4,41 @@ const resultsDiv = document.getElementById("results");
 const popup = document.getElementById("popup");
 const mealTitle = document.getElementById("meal-title");
 const mealImage = document.getElementById("meal-image");
+const mealIngredient = document.getElementById("meal-ingredient");
 const mealDescription = document.getElementById("meal-description");
 const closeButton = document.querySelector(".close-button");
 
 searchButton.addEventListener("click", async () => {
+  // 검색값을 받아옴
   const query = searchInput.value;
+  // 입력된 검색어가 있으면 실행
   if (query) {
     try {
+      // themealdb api에서 검색한 값을 fetch로 받아옴
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
-      const data = await response.json();
+      const data = await response.json(); // 받아온 값을 json 값으로 변환
       resultsDiv.innerHTML = ""; // 이전 결과 지우기
 
+      // 검색된 음식이 있다면 실행
       if (data.meals) {
+        // 검색된 음식 갯수만큼 실행해서 페이지에 뿌려줌
         data.meals.forEach((meal) => {
           const mealDiv = document.createElement("div");
           mealDiv.classList.add("result-item");
-          mealDiv.innerHTML = `<img src="${meal.strMealThumb}" alt="${meal.strMeal}" data-id="${meal.idMeal}">`;
+          mealDiv.innerHTML = `<img src="${meal.strMealThumb}" alt="${meal.strMeal}" data-id="${meal.idMeal}" class="meal-image">`;
           resultsDiv.appendChild(mealDiv);
         });
       } else {
+        // 검색된 음식이 없다면 실행
         resultsDiv.innerHTML = "<p>No Result</p>";
       }
     } catch (error) {
+      // api에서 검색한 결과가 비정상일때
       console.error("Error fetching data:", error);
       resultsDiv.innerHTML = "<p>데이터를 가져오는 중 오류가 발생했습니다.</p>";
     }
   } else {
+    // 검색값을 입력하지 않았을때
     resultsDiv.innerHTML = "<p>Search for meals of keywords</p>";
   }
 });
@@ -46,6 +55,14 @@ resultsDiv.addEventListener("click", async (event) => {
       mealTitle.innerText = meal.strMeal;
       mealImage.src = meal.strMealThumb;
       mealDescription.innerText = meal.strInstructions;
+      let ingreStr = "<b>Ingredient</b> : ";
+      for (let i = 1; i < 21; i++) {
+        let str = "strIngredient" + i;
+        if (meal[str] !== "") {
+          ingreStr += `${meal[str]}. `;
+        }
+      }
+      mealIngredient.innerHTML = ingreStr;
       popup.style.display = "block"; // 팝업 보이기
     }
   }
@@ -62,3 +79,11 @@ popup.addEventListener("click", (event) => {
     popup.style.display = "none"; // 팝업 숨기기
   }
 });
+
+// 이미지 클릭 시 새 창 열기
+// resultsDiv.addEventListener("click", async (event) => {
+//   if (event.target.classList.contains("meal-image")) {
+//     const mealId = event.target.dataset.id;
+//     window.open(`https://www.themealdb.com/meal.php?c=${mealId}`, "_blank"); // 새 창에서 열기
+//   }
+// });
