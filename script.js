c33207f317e4b843026c871877a7ee08e6233261
zinee <zinee1@naver.com>
2024-08-20 10:14:23 +0900

    검색 결과 출력시 음식 이름도 출력

--- script.js
+++ script.js
@@ -21,13 +21,29 @@ searchButton.addEventListener("click", async () => {
 
       // 검색된 음식이 있다면 실행
       if (data.meals) {
-        // 검색된 음식 갯수만큼 실행해서 페이지에 뿌려줌
+        /*
         data.meals.forEach((meal) => {
           const mealDiv = document.createElement("div");
-          mealDiv.classList.add("result-item");
-          mealDiv.innerHTML = `<img src="${meal.strMealThumb}" alt="${meal.strMeal}" data-id="${meal.idMeal}" class="meal-image">`;
+          const mealImg = document.createElement("div");
+          mealImg.classList.add("result-item");
+          mealImg.innerHTML = `<img src="${meal.strMealThumb}" alt="${meal.strMeal}" data-id="${meal.idMeal}" class="meal-image">`;
+          mealDiv.innerHTML = `${meal.strMeal}`;
+          mealDiv.appendChild(mealImg);
           resultsDiv.appendChild(mealDiv);
+        });*/
+
+        // 스트링 변수를 만들어줌(검색된 결과를 보여주는 html 코드를 저장)
+        let mealDiv = "";
+        // 검색된 음식 갯수만큼 실행해서 페이지에 뿌려줌
+        data.meals.forEach((meal) => {
+          mealDiv += `<div class='result-item'>
+            <div class='result-img'>
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" data-id="${meal.idMeal}">
+            </div>
+          <label>${meal.strMeal}</label></div>`;
         });
+        // mealDiv에 저장된 결과값을 results div에 넣어줘서 화면에 출력
+        resultsDiv.innerHTML = mealDiv;
       } else {
         // 검색된 음식이 없다면 실행
         resultsDiv.innerHTML = "<p>No Result</p>";
@@ -45,16 +61,23 @@ searchButton.addEventListener("click", async () => {
 
 // 이미지 클릭 시 팝업 열기
 resultsDiv.addEventListener("click", async (event) => {
+  // 이벤트가 발생한 타겟 요소가 <img> 태그인지 확인하여 참이면 실행
   if (event.target.tagName === "IMG") {
+    // 이벤트가 발생한 요소의 data-id 속성을 가져와 mealId라는 변수에 저장
     const mealId = event.target.dataset.id;
+    // id값으로 데이터 받아옴
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+    // json으로 변환하여 저장
     const data = await response.json();
 
+    // 해당 id로 검색한 meals 값이 있다면 실행
     if (data.meals) {
       const meal = data.meals[0];
+      // 음식 이름 출력
       mealTitle.innerText = meal.strMeal;
+      // 음식 사진 출력
       mealImage.src = meal.strMealThumb;
-      mealDescription.innerHTML = `<b>Recipe</b> : ${meal.strInstructions}`;
+      // 음식 재료에 값이 있는 만큼 재료 출력
       let ingreStr = "<b>Ingredient</b> : ";
       for (let i = 1; i < 21; i++) {
         let str = "strIngredient" + i;
@@ -63,6 +86,8 @@ resultsDiv.addEventListener("click", async (event) => {
         }
       }
       mealIngredient.innerHTML = ingreStr;
+      // 요리법 출력
+      mealDescription.innerHTML = `<b>Recipe</b> : ${meal.strInstructions}`;
       popup.style.display = "block"; // 팝업 보이기
     }
   }
